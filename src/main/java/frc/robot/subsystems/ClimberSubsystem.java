685cd625ee92ff5fe3e38e8d913a9d6c8b0e58b6
Crusaderbots <crusaderbots@gmail.com>
2022-01-26 16:15:15 -0800

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,16 +1,25 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.Constants;
 
 public class ClimberSubsystem {
 
-    public WPI_TalonFX leftFrontMotor;
-    public WPI_TalonFX leftBackMotor;
+    public WPI_TalonFX leftFixed;
+    public WPI_TalonFX rightFixed;
+    public WPI_TalonFX leftArticulating;
+    public WPI_TalonFX rightArticulating;
+
+    public Compressor compressor;
+    public DoubleSolenoid leftPiston;
+    public DoubleSolenoid rightPiston;
 
     public MotorControllerGroup mainGroup;
     
@@ -24,10 +33,21 @@ public class ClimberSubsystem {
     // public Solenoid rightSolenoid;
     
     public ClimberSubsystem() {
-        leftFrontMotor = new WPI_TalonFX(Constants.FRONT_LEFT_CLIMBER_PORT);
-        leftBackMotor = new WPI_TalonFX(Constants.BACK_LEFT_CLIMBER_PORT);
+        leftFixed = new WPI_TalonFX(Constants.LEFT_FIXED_MOTOR);
+        rightFixed = new WPI_TalonFX(Constants.RIGHT_FIXED_MOTOR);
+        leftArticulating = new WPI_TalonFX(Constants.LEFT_ARTICULATING_MOTOR);
+        rightArticulating = new WPI_TalonFX(Constants.RIGHT_ARTICULATING_MOTOR);
 
-        mainGroup = new MotorControllerGroup(leftFrontMotor, leftBackMotor);
+        leftFixed.setNeutralMode(NeutralMode.Brake);
+        rightFixed.setNeutralMode(NeutralMode.Brake);
+        leftArticulating.setNeutralMode(NeutralMode.Brake);
+        rightArticulating.setNeutralMode(NeutralMode.Brake);
+
+        compressor = new Compressor(Constants.PCM);
+        leftPiston = new DoubleSolenoid(module, moduleType, forwardChannel, reverseChannel);
+        rightPiston = new DoubleSolenoid(module, moduleType, forwardChannel, reverseChannel);
+
+        mainGroup = new MotorControllerGroup(leftArticulating, rightArticulating);
         // rightFrontMotor = new TalonFX(Constants.FRONT_RIGHT_CLIMBER_PORT);
         // rightBackMotor = new TalonFX(Constants.BACK_RIGHT_CLIMBER_PORT);
 
